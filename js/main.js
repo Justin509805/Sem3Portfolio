cf6193d8dca2f4133b6c3501d252644c406ac406
Justin509805 <509805@student.fontys.nl>
2024-09-25 16:18:17 +0200

    added js comments, created popup for learningoutcomes with links to the pages

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Open and close hamburger icon for small devices
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -7,19 +8,45 @@ function myFunction() {
     }
   }
   
-
+  // Progressbars
   window.onload = function() {
-    const percentages = [75, 70, 50, 30, 60, 85]; // Percentages for each bar
+    const scoresOutOfTen = [7, 8.5, 9, 3, 6, 8.5]; // Scores out of 10 for each bar
 
-    percentages.forEach((percentage, index) => {
+    scoresOutOfTen.forEach((score, index) => {
         const progressBar = document.getElementById(`progress-bar-${index + 1}`);
         const progressText = document.getElementById(`progress-text-${index + 1}`);
 
-        // Set the width and update the text next to each bar
+        // Makes filled in amount convert to percentage on the progressbar
+        const percentage = score * 10;
         progressBar.style.width = `${percentage}%`;
-        progressText.textContent = `${percentage}%`;
+        progressText.textContent = `${score}/10`;
     });
 };
+
+// Learning outcomes popup
+// Makes the code run dom is loaded
+document.addEventListener("DOMContentLoaded", function() {
+  // modal
+  const modal = document.getElementById('learningOutcomesModal');
+  // button on the modal
+  const openModalBtn = document.getElementById('learning-outcomes-link');
+  // x to close the modal
+  const closeModalBtn = document.querySelector('.close');
+  // open modal on click
+  openModalBtn.onclick = function() {
+      modal.style.display = "block";
+  }
+  // close the modal onclick x
+  closeModalBtn.onclick = function() {
+      modal.style.display = "none";
+  }
+  // also close when click outside of modal
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  }
+});
 
 
 
